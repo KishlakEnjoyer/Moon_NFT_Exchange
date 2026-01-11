3a903b7f0255b08d295b14be2129051f08d269f7
Erik <erik.fayzullin.2007@mail.ru>
2026-01-11 05:07:12 +0500

    Begin to develop Home Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './styles/App.css';
-import { ConfigProvider, theme } from 'antd';
+import { ConfigProvider, Layout, theme } from 'antd';
 import { Routes, Route } from 'react-router-dom';
+import MainView from './views/MainView';
+import { Content } from 'antd/es/layout/layout';
 
 
 function App() {
   return (
     <ConfigProvider theme={{
-      algorithm: theme.darkAlgorithm
+      algorithm: theme.darkAlgorithm,
+      //algorithm: theme.defaultAlgorithm,
+      token: {
+        fontFamily: 'Geist, sans-serif',
+      },
     }}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<div>Главная страница</div>} />
-          <Route path="*" element={<div>404 — Страница не найдена</div>} />
-        </Routes>
-      </div>
+      <Layout className='App'>
+          <Routes>
+            <Route path="/" element={<MainView />} />
+            <Route path="*" element={<div>404 — Страница не найдена</div>} />
+          </Routes>
+      </Layout>
     </ConfigProvider>
   );
 }
