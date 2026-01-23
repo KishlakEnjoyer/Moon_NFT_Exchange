e9b5673bbcf2a5cb1b7257159a87907fe15211ba
KishlakEnjoyer <erik.fayzullin.2007@mail.ru>
2026-01-23 08:18:36 +0500

    Fix logo color by theme

--- frontend/src/views/MainView.tsx
+++ frontend/src/views/MainView.tsx
@@ -1,29 +1,13 @@
 import "../styles/MainViewStyle.css";
 import useDocumentTitle from "../hooks/useDocumentTitle";
-import { Button, Layout } from "antd";
+import { FloatButton, Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
 import MainList from "../components/MainList";
 import FilterBar from "../components/FilterBar";
-import { useEffect, useState } from "react";
 import { UpOutlined } from "@ant-design/icons";
 
 const MainView = () => {
   useDocumentTitle("Moon Exchange - Home");
-
-  const [showScrollTop, setShowScrollTop] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setShowScrollTop(window.scrollY > 400);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -32,13 +16,7 @@ const MainView = () => {
           <FilterBar />
         </div>
         <MainList />
-        {showScrollTop && (
-          <Button
-            onClick={scrollToTop}
-            className="button-up"
-            icon={<UpOutlined />}
-          />
-        )}
+        <FloatButton.BackTop icon={<UpOutlined/>} style={{ right: '10px', bottom: '10px' }} shape="square"/>
       </Content>
     </Layout>
   );
